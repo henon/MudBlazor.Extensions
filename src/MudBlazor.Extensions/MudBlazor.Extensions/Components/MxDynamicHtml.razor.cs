f0366cb0d494c94261d18a10d0d9f9391a746e8d
Meinrad Recheis <meinrad.recheis@gmail.com>
2021-01-08 10:17:31 +0100

    Added MudBlazor.Extensions.Docs.Server as proof of concept

---
+++ src/MudBlazor.Extensions/MudBlazor.Extensions/Components/MxDynamicHtml.razor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MudBlazor.Extensions
+{
+    public partial class MxDynamicHtml : MudComponentBase
+    {
+
+        [Parameter] public string Html { get; set; }
+        
+        RenderFragment DynamicRenderFragment()
+        {
+            return new RenderFragment(builder =>
+            {
+                builder.AddMarkupContent(0, Html);
+            });
+        }
+        
+    }
+}
