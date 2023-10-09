f2eafeff0f1a36add3896ea1d9ebdf98f9421231
Florian Gilde <fgilde@gmail.com>
2023-10-09 12:02:57 +0200

    Add small sample for MudBlazor.Extensions

---
+++ src/MudBlazor.Extensions.Explorer/Extensions/ThirdParty/MudEx/MudEx.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using MudBlazor.Extensions.Explorer.Models;
+
+namespace MudBlazor.Extensions.Explorer.Extensions
+{
+    [Extension]
+    public class MudExExtension : MudExtension
+    {
+        public override string Name => "MudBlazor.Extensions";
+        public override string FullName => "MudBlazor.Extensions";
+        public override string Description => "MudBlazor.Extensions is a convenient package that extends the capabilities of the MudBlazor component library with more components, fluent form generation, dialog extensions and more";
+        public override string Authors => "fgilde";
+        public override string License => "MIT";
+        public override bool IsThirdParty => true;
+        public override string GithubRepo => "https://github.com/fgilde/MudBlazor.Extensions";
+        public override string Nuget => "https://www.nuget.org/packages/MudBlazor.Extensions/";
+
+        public override IEnumerable<Type> Docs
+        {
+            get
+            {
+                yield return typeof(MudExDocs);
+            }
+        }
+    }
+}
